4af0bdb4d7b0d95af7dc4964220c12bdf06f3494
MPE <info@ciaompe.com>
2018-03-25 13:24:21 +0530

    Slim example HomeController

---
+++ HomeController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Controllers;
+
+/**
+*  Base Controller
+*/
+
+use Slim\Views\Twig;
+
+/*
+PHP DI
+ */
+use Interop\Container\ContainerInterface;
+
+
+/*
+Slim Router
+ */
+use Slim\Interfaces\RouterInterface;
+
+
+class HomeController
+{
+	protected $container, $router, $view, $flash;
+
+	public function __construct(ContainerInterface $c, RouterInterface $router, Twig $view){
+		$this->container = $c;
+		$this->router = $router;
+		$this->view = $view;
+	}
+
+	public function test(Request $request, Response $response,) {
+		echo $this->container->get('lang');
+		
+	}
+	
+}
